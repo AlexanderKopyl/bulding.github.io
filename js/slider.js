821e1e50189fb5034f3fc830cf19c6417f6a7240
alexsander <alexsander@gmai.com>
2018-03-31 14:04:11 +0300

    first commit

---
+++ js/slider.js
@@ -0,0 +1,64 @@
+var slideNow = 2;
+var slideCount = $('#slidewrapper').children().length;
+var translateWidth = 0;
+var equally = 0;
+var slide = 0;
+var slideunder = 0;
+if($('#viewport').width() === 320){
+    slideNow = 1;
+    equally = 1;
+    slide = 1;
+    slideunder = 2;
+}else{
+    slideNow = 2;
+    equally = 2;
+    slide = 5;
+    slideunder = 6;
+};
+
+
+function prevSlide() {
+    if (slideNow === equally || slideNow <= 0 || slideNow > slideCount) {
+        translateWidth = -$('#viewport').width() * (slideCount - slide);
+        $('#slidewrapper').css({
+            'transform': 'translate(' + translateWidth + 'px, 0)',
+            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+            '-ms-transform': 'translate(' + translateWidth + 'px, 0)'
+        });
+        slideNow = slideCount;
+    } else {
+        translateWidth = -$('#viewport').width() * (slideNow - slideunder);
+        $('#slidewrapper').css({
+            'transform': 'translate(' + translateWidth + 'px, 0)',
+            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+            '-ms-transform': 'translate(' + translateWidth + 'px, 0)'
+        });
+        slideNow-- ;
+        if(slideNow === slide ){
+            slideNow = equally;
+        }
+    }
+}
+function nextSlide() {
+
+    if (slideNow === slideCount || slideNow <= 0 || slideNow > slideCount) {
+        $('#slidewrapper').css('transform', 'translate(0, 0)');
+        slideNow = equally;
+    } else {
+        translateWidth = -$('#viewport').width() * (slideNow/equally);
+        $('#slidewrapper').css({
+            'transform': 'translate(' + translateWidth + 'px, 0)',
+            '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+            '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+        });
+        slideNow+=equally;
+    }
+}
+$('#next-btn').click(function() {
+
+    nextSlide();
+});
+
+$('#prev-btn').click(function() {
+    prevSlide();
+});
