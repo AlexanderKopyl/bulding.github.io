3d0dad57bd649002a11d19045cf250e0176ad725
alexsander <alexsander@gmai.com>
2018-04-04 21:19:32 +0300

    Finish

--- modal.php
+++ modal.php
@@ -2,14 +2,14 @@
 
 $name = $_POST["name"];
 $email = $_POST["email"];
-$adress = $_POST["adress"];
+$textArea = $_POST["textArea"];
 $tel = $_POST["tel"];
 $mailheaders = "Content-type: text/html; charset=UTF-8 \r\n";
 $mailheaders .= "From: fire.co.ua <no-reply@fire.co.ua>";
 $mailheaders .= "Reply-To: fire.co.ua";
 $adminEmail = "anast.nosal@gmail.com";
-$text = "<p>Ваше имя :". $_POST['name']. "</p><p> Ваш Телефон: ".$_POST['tel']. " </p><p>Ваш Email: ".$_POST['email']. "</p><p>Ваш Adress:" .$_POST['adress']."</p><br><p>Вы зарегистрировались на обратный звонк , через время вас наберут</p>";
-$textForAdmin = "<p>Имя Пользователя :". $_POST['name']. "</p><p>Телефон пользователя: ".$_POST['tel']. " </p><p>Email пользователя: ".$_POST['email']. "</p><p>Adress пользователя:" .$_POST['adress']."</p><br><p>Свяжитесь с клиентом для выяснения нюансов</p>";
+$text = "<p>Ваше имя :". $_POST['name']. "</p><p> Ваш Телефон: ".$_POST['tel']. " </p><p>Ваш Email: ".$_POST['email']. "</p><p>Ваш Коментарий:" .$_POST['textArea']."</p><br><p>Вы зарегистрировались на обратный звонк , через время вас наберут</p>";
+$textForAdmin = "<p>Имя Пользователя :". $_POST['name']. "</p><p>Телефон пользователя: ".$_POST['tel']. " </p><p>Email пользователя: ".$_POST['email']. "</p><p>Коментарий пользователя:" .$_POST['textArea']."</p><br><p>Свяжитесь с клиентом для выяснения нюансов</p>";
 mail($email,"Form Test site",$text,$mailheaders);
 mail($adminEmail,"Form Test site",$textForAdmin,$mailheaders);
 header('Location:index.html');
